d165e95be37d2a4ba5a26c1ff4171d0dba3879e9
Noah <noahjames1035@gmail.com>
2025-05-28 13:19:31 -0400

    countdown solution

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -7,6 +7,8 @@ import Slider from "react-slick";
 
 const NewItems = () => {
   const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
     async function fetchPost() {
@@ -15,6 +17,9 @@ const NewItems = () => {
       );
       console.log(data);
       setPost(data);
+      setTimeout(() => {
+        setLoading(false);
+      }, 5000);
     }
     fetchPost();
   }, []);
@@ -27,6 +32,8 @@ const NewItems = () => {
     slidesToScroll: 1,
   };
 
+  function startTimer() {
+  }
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -39,60 +46,80 @@ const NewItems = () => {
           </div>
           <Slider {...settings}>
             {post.map((posts) => (
-              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
-                <div className="nft__item">
-                  <div className="author_list_pp">
-                    <Link
-                      to="/author"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="Creator: Monica Lucas"
-                    >
-                      <img className="lazy" src={posts.authorImage} alt="" />
-                      <i className="fa fa-check"></i>
-                    </Link>
+              <>
+                {loading ? (
+                  <div className="skeleton">
+                    <div className="item__wrap--skeleton">
+                      <div className="pfp__skeleton">
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="countdown__skeleton"></div>
+                      <div className="background__skeleton"></div>
+                      <div className="title__skeleton"></div>
+                      <div className="price-likes__skeleton"></div>
+                    </div>
                   </div>
-                  <div className="de_countdown">{posts.expiryDate}</div>
+                ) : (
+                  <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                    <div className="nft__item">
+                      <div className="author_list_pp">
+                        <Link
+                          to="/author"
+                          data-bs-toggle="tooltip"
+                          data-bs-placement="top"
+                          title={posts.title}
+                        >
+                          <img
+                            className="lazy"
+                            src={posts.authorImage}
+                            alt=""
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="de_countdown">{posts.expiryDate}</div>
+                      
+                      <div className="nft__item_wrap">
+                        <div className="nft__item_extra">
+                          <div className="nft__item_buttons">
+                            <button>Buy Now</button>
+                            <div className="nft__item_share">
+                              <h4>Share</h4>
+                              <a href="" target="_blank" rel="noreferrer">
+                                <i className="fa fa-facebook fa-lg"></i>
+                              </a>
+                              <a href="" target="_blank" rel="noreferrer">
+                                <i className="fa fa-twitter fa-lg"></i>
+                              </a>
+                              <a href="">
+                                <i className="fa fa-envelope fa-lg"></i>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
 
-                  <div className="nft__item_wrap">
-                    <div className="nft__item_extra">
-                      <div className="nft__item_buttons">
-                        <button>Buy Now</button>
-                        <div className="nft__item_share">
-                          <h4>Share</h4>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-facebook fa-lg"></i>
-                          </a>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-twitter fa-lg"></i>
-                          </a>
-                          <a href="">
-                            <i className="fa fa-envelope fa-lg"></i>
-                          </a>
+                        <Link to="/item-details">
+                          <img
+                            src={posts.nftImage}
+                            className="lazy nft__item_preview"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+                      <div className="nft__item_info">
+                        <Link to="/item-details">
+                          <h4>{posts.title}</h4>
+                        </Link>
+                        <div className="nft__item_price">{posts.price} ETH</div>
+                        <div className="nft__item_like">
+                          <i className="fa fa-heart"></i>
+                          <span>{posts.likes}</span>
                         </div>
                       </div>
                     </div>
-
-                    <Link to="/item-details">
-                      <img
-                        src={posts.nftImage}
-                        className="lazy nft__item_preview"
-                        alt=""
-                      />
-                    </Link>
                   </div>
-                  <div className="nft__item_info">
-                    <Link to="/item-details">
-                      <h4>{posts.title}</h4>
-                    </Link>
-                    <div className="nft__item_price">{posts.price} ETH</div>
-                    <div className="nft__item_like">
-                      <i className="fa fa-heart"></i>
-                      <span>{posts.likes}</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
+                )}
+              </>
             ))}
           </Slider>
         </div>
